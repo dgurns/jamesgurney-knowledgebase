c3db4fa14fb2aaba37d3b57221cdd0ccc976a6f1
Dan Gurney <dan@dangurney.net>
2023-04-07 00:31:41 +0100

    feat: experiment with prompt. Add Python script to fetch blog posts

--- env.sample.sh
+++ env.sample.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# how to use:
+#  $ cp env.sample.sh env.sh
+#  $ chmod +x env.sh
+# fill out env values in env.sh
+#  $ source env.sh
+
 export DATASTORE=pinecone
 export BEARER_TOKEN=
 export OPENAI_API_KEY=
--- upsert-gurneyjourney-blog-posts.py
+++ upsert-gurneyjourney-blog-posts.py
@@ -1,15 +1,16 @@
 import os
 import re
 import requests
-from bs4 import BeautifulSoup
-from urllib.parse import urlparse, parse_qs
+from bs4 import BeautifulSoup, Tag
+from typing import Union
 
 
-def fetch_posts(start_url):
+def fetch_posts(start_url: str):
     posts = []
-    next_page_url = start_url
+    next_page_url: Union[str, None] = start_url
 
     while next_page_url:
+        print(f"So far, have {len(posts)} posts")
         print(f"Fetching posts from page: {next_page_url}")
         response = requests.get(next_page_url)
         # if response is not 200, print error and continue to next page
@@ -18,16 +19,21 @@ def fetch_posts(start_url):
         print(f"Parsing posts from page: {next_page_url}")
 
         for element in soup.find_all('div', class_='post'):
-            url = element.find(
-                class_='post-title').find('a').get('href')
-            post_id = re.sub(r'\W+', '', url)
-            text = element.find(class_='post-body').text.strip()
-            posts.append({'id': post_id, 'text': text, 'url': url})
+            try:
+                url = element.find(
+                    class_='post-title').find('a').get('href')
+                post_id = re.sub(r'\W+', '', url)
+                text = element.find(class_='post-body').text.strip()
+                posts.append({'id': post_id, 'text': text, 'url': url})
+            except AttributeError:
+                pass
 
         # Find the next page URL
         older_posts_link = soup.find('a', class_='blog-pager-older-link')
-        if older_posts_link:
-            next_page_url = older_posts_link.get('href')
+        if isinstance(older_posts_link, Tag):
+            older_href = older_posts_link.get('href')
+            if isinstance(older_href, str):
+                next_page_url = older_href
         else:
             next_page_url = None
 
@@ -57,6 +63,7 @@ if __name__ == '__main__':
     start_url = 'https://gurneyjourney.blogspot.com'
 
     posts = fetch_posts(start_url)
+    print(f"Fetched {len(posts)} posts from Blogspot")
 
     for post in posts:
         response = send_post_to_upsert(post)

b53a9508b831f02e79e1474fd0b99343c007c601
Dan Gurney <dan@dangurney.net>
2023-04-06 12:29:27 +0100

    feat: add set-env script

---
+++ set-env.sample.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export DATASTORE=pinecone
+export BEARER_TOKEN=
+export OPENAI_API_KEY=
+export PINECONE_API_KEY=
+export PINECONE_ENVIRONMENT=
+export PINECONE_INDEX=
+
+echo "Environment variables set successfully"

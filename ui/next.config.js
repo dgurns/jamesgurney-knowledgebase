87e3fbf61124a482e806cea410790b388da40f1d
Dan Gurney <dan@dangurney.net>
2026-06-04 16:08:14 +0100

    redirect all requests to jamesgurney.com

--- ui/next.config.js
+++ ui/next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://jamesgurney.com',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
